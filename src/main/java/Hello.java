f24447a1bae921d3b661ea7cc65793dc9be40e49
Сергей Шулыма <C:\Users\\AppData\Roaming\The Bat!\Mail>
2018-11-09 00:34:39 +0200

    Master branch

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,8 +2,8 @@
 public class Hello {
 
 	public static void main(String[] args) {
-       for (int i=0; i<=5; i++) {
-       System.out.println("Hello World!!!");
-	   }
+      
+       System.out.println("Master Branch of git!!!");
+	  
 	}
 }
