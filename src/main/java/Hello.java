757698b18b833fbf443b67b94257aa8e482004a5
Сергей Шулыма <C:\Users\\AppData\Roaming\The Bat!\Mail>
2018-11-09 11:38:16 +0200

    automatic closing the file

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 
 	public static void main(String[] args) {
  
-       ReadFile rf= new ReadFile();
-       System.out.println(rf);
+      
+      System.out.println(new ReadFile());
 	}
 }
--- src/main/java/ReadFile.java
+++ src/main/java/ReadFile.java
@@ -2,10 +2,9 @@ import java.io.*;
 
 public class ReadFile {
 	int i;
-	FileInputStream fin = null;
+	
   {
-	try {
-		fin = new FileInputStream ("D:\\Workspace\\FFANS_Project\\ffans_project\\File_Ini\\text.ini");
+	try (FileInputStream fin =new FileInputStream("D:\\Workspace\\FFANS_Project\\ffans_project\\File_Ini\\text.ini")){
 		do {
 			i=fin.read();
 			if (i!=-1)
@@ -14,12 +13,7 @@ public class ReadFile {
 		while(i!=-1);
 	} catch (IOException e) {
 		System.out.println ("Input-Output Exception" + e);
-	} finally {
-		try {
-			if (fin != null) fin.close();
-		}catch (IOException e2) {
-			System.out.println("Output file closing Exception"+ e2);
-		}
-	}
+	} 
+	
   }
 }
