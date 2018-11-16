ca81601b7fc8c7a4fe87ccadf117aac3c405391f
Сергей Шулыма <C:\Users\\AppData\Roaming\The Bat!\Mail>
2018-11-16 14:00:17 +0200

    Reading parameters from .ini file

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,14 +2,9 @@
 public class Hello {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-      
-       System.out.println("Master Branch of git!!!");
-	  
-=======
- 
+
       
       new ReadFile();
->>>>>>> develop
+
 	}
 }
